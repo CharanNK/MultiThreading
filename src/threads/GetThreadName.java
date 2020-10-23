951d6077dca3d965e47d9727f74d1fa7b14320b0
CharanNK <charkeet@gmail.com>
2020-10-23 23:45:33 +0530

    thread sleeps

--- src/threads/GetThreadName.java
+++ src/threads/GetThreadName.java
@@ -6,10 +6,21 @@ public class GetThreadName {
 		Runnable runnable = ()->{
 			String threadName = Thread.currentThread().getName();
 			System.out.println("running "+threadName);
+			
+			try {
+				Thread.sleep(2000);
+			}catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+			
+			System.out.println(threadName+" finished");
 		};
 		
 		Thread thread = new Thread(runnable, "test");
 		thread.start();
+		
+		Thread thread2 = new Thread(runnable,"thread2 ");
+		thread2.start();
 	}
 
 }
